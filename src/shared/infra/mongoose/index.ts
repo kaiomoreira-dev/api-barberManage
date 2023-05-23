93dbc468b915d886420794e00f22ba1e991c16a3
Kaio Moreira <kaio-dev@outlook.com>
2023-05-23 15:30:44 -0300

    create file connection for mongoDB

---
+++ src/shared/infra/mongoose/index.ts
@@ -0,0 +1,29 @@
+/* eslint-disable import/export */
+import { AppError } from "@shared/errors/AppError";
+import "dotenv/config";
+
+import mongoose, { Connection } from "mongoose";
+
+export default async function connectionMongoDB(): Promise<Connection> {
+    try {
+        const url =
+            process.env.NODE_ENV === "test"
+                ? process.env.MONGODB_URL_DBTEST
+                : process.env.MONGODB_URL_BARBERSHOP;
+
+        await mongoose.connect(url);
+
+        const { connection } = mongoose;
+
+        connection.on("open", () => {
+            console.log("Conectado ao MongoDB com sucesso!");
+        });
+
+        return connection;
+    } catch (error) {
+        console.log(error.message);
+        throw new AppError("Erro ao conectar ao MongoDB");
+    }
+}
+
+connectionMongoDB();
