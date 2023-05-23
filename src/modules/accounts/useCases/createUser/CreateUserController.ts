388471388d99fa69ccd534b0936d0266427622ed
Kaio Moreira <kaio-dev@outlook.com>
2023-05-23 15:16:35 -0300

    create controller for receive atributes user and return response

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+export class CreateUserController {
+    async hundle(request: Request, response: Response): Promise<Response> {
+        const { name, email, password, address } = request.body;
+
+        const createUserUseCase = container.resolve(CreateUserUseCase);
+
+        await createUserUseCase.execute({
+            name,
+            email,
+            password,
+            address,
+        });
+
+        return response
+            .status(201)
+            .json({ message: "User created successfully" });
+    }
+}
