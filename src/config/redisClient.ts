ff540faa1df690955f5d55046d0f213aafd70a15
Kaio Moreira <kaio-dev@outlook.com>
2023-05-23 15:10:37 -0300

    create conection for redis and export

---
+++ src/config/redisClient.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { createClient } from "redis";
+
+export const redisClient = createClient({
+    host:
+        process.env.NODE_ENV === "test" ? "localhost" : process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+});
+
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
