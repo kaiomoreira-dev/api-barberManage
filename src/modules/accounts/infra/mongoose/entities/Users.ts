c6ccaf8f95fe613fa15924c01efc125978629424
Kaio Moreira <kaio-dev@outlook.com>
2023-05-23 15:14:33 -0300

    create schema entitie User

---
+++ src/modules/accounts/infra/mongoose/entities/Users.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Schema, model, Document } from "mongoose";
+
+export interface IUserModel extends Document {
+    name: string;
+    email: string;
+    password: string;
+    address: string;
+    admin: boolean;
+    employee: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export const UserSchema = new Schema<IUserModel>({
+    name: { type: String, required: true },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^\S+@\S+\.\S+$/,
+    },
+    password: { type: String, required: true },
+    address: { type: String, required: false },
+    admin: { type: Boolean, default: false },
+    employee: { type: Boolean, default: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
+
+export default model<IUserModel>("Users", UserSchema);
