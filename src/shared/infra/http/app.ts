beaa9dea8ce2c6affca8301ba5fbd2ad052128f6
Kaio Moreira <kaio-dev@outlook.com>
2023-05-23 15:26:59 -0300

    create app with all configuration for API

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import express, { NextFunction, Request, Response } from "express";
+
+import "dotenv/config";
+
+import "reflect-metadata";
+
+import "express-async-errors";
+
+import { AppError } from "@shared/errors/AppError";
+
+import "@shared/container";
+
+import { router } from "./routes";
+
+export const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+        if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+                message: err.message,
+            });
+        }
+        return response.status(500).json({
+            status: "error",
+            message: `Internal server - error: ${err.message}`,
+        });
+    }
+);
