4cdd2f5f248d74ebb586283016859b22cbbca02c
Kaio Moreira <kaio-dev@outlook.com>
2023-05-23 22:32:38 -0300

    create manage route company

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 
+import { companysRoutes } from "./companys.routes";
 import { usersRoutes } from "./users.routes";
 
 export const router = Router();
 
 router.use("/api/users", usersRoutes);
+router.use("/api/companys", companysRoutes);
