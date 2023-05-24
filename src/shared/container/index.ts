a53271bec0a3af3236e87d048d576bf07061e986
Kaio Moreira <kaio-dev@outlook.com>
2023-05-23 22:33:24 -0300

    create container for intance company

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { UsersRepository } from "@modules/accounts/infra/mongoose/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CompanysRepository } from "@modules/companys/infra/mongoose/repositories/CompanysRepository";
 import { container } from "tsyringe";
 import "./in-memory";
+import { ICompanysRepository } from "@modules/companys/repositories/ICompanysRepository";
 
 container.register<IUsersRepository>("UsersRepository", {
     useClass: UsersRepository,
+});
+
+container.register<ICompanysRepository>("CompanysRepository", {
+    useClass: CompanysRepository,
 });
