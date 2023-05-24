e49b7a72db17f797ac6ede2854ce0419662cba12
Kaio Moreira <kaio-dev@outlook.com>
2023-05-23 22:36:37 -0300

    create interface repository for Companys with method create, find by and name, list, delete and update by id

---
+++ src/modules/companys/repositories/ICompanysRepository.ts
@@ -0,0 +1,15 @@
+import { ICreateCompanysDTO } from "../dtos/ICreateCompanysDTO";
+import { ICompanyModel } from "../infra/mongoose/entities/Companys";
+
+export interface ICompanysRepository {
+    create(data: ICreateCompanysDTO): Promise<ICompanyModel>;
+
+    list(): Promise<ICompanyModel[]>;
+
+    findById(id: string): Promise<ICompanyModel>;
+    findByName(name: string): Promise<ICompanyModel>;
+
+    updateById(data: ICreateCompanysDTO): Promise<void>;
+
+    deleteById(id: string): Promise<void>;
+}
