305d7fbce474a956406338a36c42f481ed708623
Kaio Moreira <kaio-dev@outlook.com>
2023-05-27 11:52:05 -0300

    create entitie Service with using moongonse

---
+++ src/modules/services/infra/mongoose/entities/Services.ts
@@ -0,0 +1,18 @@
+import { ObjectId } from "mongodb";
+import { Schema, model, Document } from "mongoose";
+
+export interface IServiceModel extends Document {
+    idCompanys: ObjectId;
+    description: string;
+    name: string;
+    price: number;
+}
+
+export const ServiceSchema = new Schema<IServiceModel>({
+    idCompanys: { type: ObjectId, required: true },
+    description: { type: String, required: false },
+    name: { type: String, required: true },
+    price: { type: Number, required: true, default: 0 },
+});
+
+export default model<IServiceModel>("Services", ServiceSchema);
