61d4adc57d3daabdadd41d415eadc0f34da6390d
Kaio Moreira <kaio-dev@outlook.com>
2023-05-27 11:50:45 -0300

    create route index for service

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { companysRoutes } from "./companys.routes";
+import { servicesRoutes } from "./services.routes";
 import { usersRoutes } from "./users.routes";
 
 export const router = Router();
 
 router.use("/api/users", usersRoutes);
 router.use("/api/companys", companysRoutes);
+router.use("/api/services", servicesRoutes);
