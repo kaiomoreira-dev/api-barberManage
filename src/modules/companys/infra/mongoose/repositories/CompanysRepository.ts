8d73c5be4dc774d4dfccb8eb8a0f21f94b12547c
Kaio Moreira <kaio-dev@outlook.com>
2023-05-29 16:43:17 -0300

    remove improts

--- src/modules/companys/infra/mongoose/repositories/CompanysRepository.ts
+++ src/modules/companys/infra/mongoose/repositories/CompanysRepository.ts
@@ -1,73 +1,85 @@
 /* eslint-disable no-return-await */
-import { ICreateCompanysDTO } from '@modules/companys/dtos/ICreateCompanysDTO';
-import { ICompanysRepository } from '@modules/companys/repositories/ICompanysRepository';
-import { Model } from 'mongoose';
-import { injectable } from 'tsyringe';
+import { ICreateCompanysDTO } from "@modules/companys/dtos/ICreateCompanysDTO";
+import { ICompanysRepository } from "@modules/companys/repositories/ICompanysRepository";
+import { ObjectId } from "mongodb";
+import { Model } from "mongoose";
+import { injectable } from "tsyringe";
 
-import { AppError } from '@shared/errors/AppError';
+import { AppError } from "@shared/errors/AppError";
 
-import Companys, { ICompanyModel } from '../entities/Companys';
+import Companys, { ICompanyModel } from "../entities/Companys";
 
 @injectable()
 export class CompanysRepository implements ICompanysRepository {
-	private repository: Model<ICompanyModel>;
+    private repository: Model<ICompanyModel>;
 
-	constructor() {
-		this.repository = Companys;
-	}
-	async findByName(name: string): Promise<ICompanyModel> {
-		return await this.repository.findOne({ name });
-	}
-	async create({ idUsers, name, address, phone }: ICreateCompanysDTO): Promise<ICompanyModel> {
-		try {
-			const companys = await this.repository.create({
-				idUsers,
-				name,
-				address,
-				phone,
-			});
+    constructor() {
+        this.repository = Companys;
+    }
+    async findByName(name: string): Promise<ICompanyModel> {
+        return await this.repository.findOne({ name });
+    }
+    async create({
+        idUsers,
+        name,
+        address,
+        phone,
+    }: ICreateCompanysDTO): Promise<ICompanyModel> {
+        try {
+            const companys = await this.repository.create({
+                idUsers,
+                name,
+                address,
+                phone,
+            });
 
-			return companys;
-		} catch (error) {
-			console.log(error.message);
-			throw new AppError('Error creating company');
-		}
-	}
-	async list(): Promise<ICompanyModel[]> {
-		try {
-			return this.repository.find();
-		} catch (error) {
-			console.log(error.message);
-			throw new AppError('Error listing companys');
-		}
-	}
-	async findById(id: string): Promise<ICompanyModel> {
-		try {
-			return this.repository.findById(id);
-		} catch (error) {
-			console.log(error.message);
-			throw new AppError('Error find company');
-		}
-	}
-	async updateById({ id, idUsers, name, address, phone }: ICreateCompanysDTO): Promise<void> {
-		try {
-			await this.repository.findByIdAndUpdate(id, {
-				idUsers,
-				name,
-				address,
-				phone,
-			});
-		} catch (error) {
-			console.log(error.message);
-			throw new AppError('Error update company');
-		}
-	}
-	async deleteById(id: string): Promise<void> {
-		try {
-			await this.repository.findByIdAndDelete(id);
-		} catch (error) {
-			console.log(error.message);
-			throw new AppError('Error delete company');
-		}
-	}
+            return companys;
+        } catch (error) {
+            console.log(error.message);
+            throw new AppError("Error creating company");
+        }
+    }
+    async list(): Promise<ICompanyModel[]> {
+        try {
+            return this.repository.find();
+        } catch (error) {
+            console.log(error.message);
+            throw new AppError("Error listing companys");
+        }
+    }
+    async findById(id: string): Promise<ICompanyModel> {
+        try {
+            return this.repository.findById(id);
+        } catch (error) {
+            console.log(error.message);
+            throw new AppError("Error find company");
+        }
+    }
+    async updateById({
+        id,
+        idUsers,
+        name,
+        address,
+        phone,
+    }: ICreateCompanysDTO): Promise<void> {
+        try {
+            await this.repository.findByIdAndUpdate(id, {
+                idUsers,
+                name,
+                address,
+                phone,
+            });
+        } catch (error) {
+            console.log(error.message);
+            throw new AppError("Error update company");
+        }
+    }
+    async deleteById(id: string): Promise<void> {
+        try {
+            await this.repository.findByIdAndDelete(id);
+        } catch (error) {
+            console.log(error.message);
+            throw new AppError("Error delete company");
+        }
+    }
 }
