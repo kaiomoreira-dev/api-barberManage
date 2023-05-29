6ead536fd127c46003d1060c729e5cbbddebd476
Kaio Moreira <kaio-dev@outlook.com>
2023-05-29 16:08:30 -0300

    create use case for update Client by ID

---
+++ src/modules/clients/useCases/updateClientById/UpdateClientByIdController.ts
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { UpdateClientByIdUseCase } from "./UpdateClientByIdUseCase";
+
+export class UpdateClientByIdController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { name, address, esqd, num, pg, phone } = request.body;
+        const { idClients } = request.params;
+
+        const updateClientByIdUseCase = container.resolve(
+            UpdateClientByIdUseCase
+        );
+
+        await updateClientByIdUseCase.execute({
+            id: idClients,
+            name,
+            address,
+            esqd,
+            num,
+            pg,
+            phone,
+        });
+
+        return response
+            .status(200)
+            .json({ message: "Updated Client successfully." });
+    }
+}
