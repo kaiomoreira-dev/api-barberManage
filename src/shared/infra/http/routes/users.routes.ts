3c6b9718250e86f4fee73994d114aba8b9dd90bb
Kaio Moreira <kaio-dev@outlook.com>
2023-05-29 16:10:30 -0300

    create route for list all user for company Id, add ensuresAdmin, ensureAuthenticate and create route for update user

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,8 +1,28 @@
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { ListUsersByCompanyIdController } from "@modules/accounts/useCases/listUserByCompaniId/ListUsersByCompaniIdController";
+import { UpdateUsersController } from "@modules/accounts/useCases/updateUser/UpdateUsersController";
 import { Router } from "express";
+
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticate } from "../middlewares/ensureAuthenticate";
 
 export const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
 
+const listUsersByCompanyIdController = new ListUsersByCompanyIdController();
+
+const updateUsersController = new UpdateUsersController();
+
 usersRoutes.post("/", createUserController.handle);
+
+usersRoutes.get(
+    "/:idCompanys",
+    ensureAuthenticate,
+    listUsersByCompanyIdController.handle
+);
+
+usersRoutes.put("/", ensureAuthenticate, updateUsersController.handle);
+
+// criar rota para atualizar qualquer user para administrador
+// Mas com apenas permissão de desenvolvedor
