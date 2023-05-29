d27270c975475cd78236cb5c9527a6da3bde97aa
Kaio Moreira <kaio-dev@outlook.com>
2023-05-29 15:57:14 -0300

    create use case for list all service with idCompanys

---
+++ src/modules/services/useCases/listServicesByCompanyId/ListServicesByCompanyIdUseCase.ts
@@ -0,0 +1,30 @@
+import { ICompanysRepository } from "@modules/companys/repositories/ICompanysRepository";
+import { IServiceModel } from "@modules/services/infra/mongoose/entities/Services";
+import { IServicesRepository } from "@modules/services/repositories/IServicesRepository";
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@shared/errors/AppError";
+
+@injectable()
+export class ListServicesByCompanyIdUseCase {
+    constructor(
+        @inject("ServicesRepository")
+        private servicesRepository: IServicesRepository,
+        @inject("CompanysRepository")
+        private companysRepository: ICompanysRepository
+    ) {}
+
+    async execute(idCompanys: string): Promise<IServiceModel[]> {
+        const checkServiceExists = await this.companysRepository.findById(
+            idCompanys
+        );
+
+        if (!checkServiceExists) {
+            throw new AppError("Company not found", 404);
+        }
+
+        const companys = await this.servicesRepository.list();
+
+        return companys;
+    }
+}
