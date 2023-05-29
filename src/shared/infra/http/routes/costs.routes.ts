7d7584053f859c0d8317f4fd26e63ab6c97af512
Gustavo Reimche <gustavoreimche@hotmail.com>
2023-05-29 17:51:14 -0300

    add route of findById and deleteById

--- src/shared/infra/http/routes/costs.routes.ts
+++ src/shared/infra/http/routes/costs.routes.ts
@@ -1,4 +1,6 @@
 import { CreateCostController } from '@modules/costs/useCases/createCost/CreateCostController';
+import { DeleteCostByIdController } from '@modules/costs/useCases/deleteById/DeleteCostByIdController';
+import { FindCostByIdController } from '@modules/costs/useCases/findById/FindByIdController';
 import { ListByIdCompanysController } from '@modules/costs/useCases/listByIdCompanys/ListByIdCompanysController';
 
 import { Router } from 'express';
@@ -7,6 +9,10 @@ export const costRoutes = Router();
 
 const createCostController = new CreateCostController();
 const listByIdCompanysController = new ListByIdCompanysController();
+const findByIdController = new FindCostByIdController();
+const deleteCostByIdController = new DeleteCostByIdController();
 
 costRoutes.post('/:idCompanys', createCostController.handle);
-costRoutes.get('/:idCompanys', listByIdCompanysController.handle);
+costRoutes.get('/:idCost', findByIdController.handle);
+costRoutes.get('/idCompany/:idCompanys', listByIdCompanysController.handle);
+costRoutes.delete('/:idCost', deleteCostByIdController.handle);
