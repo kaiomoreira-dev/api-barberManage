5fa622fa6dbab3095bb97c4b984d77a96445278c
Kaio Moreira <kaio-dev@outlook.com>
2023-05-29 16:11:23 -0300

    add middlewares for authenticate routes for only admin acess

--- src/shared/infra/http/routes/services.routes.ts
+++ src/shared/infra/http/routes/services.routes.ts
@@ -1,10 +1,13 @@
 import { CreateServiceController } from "@modules/services/useCases/createService/CreateServiceController";
 import { DeleteServiceByIdController } from "@modules/services/useCases/deleteServiceById/DeleteServiceByIdController";
-import { FindServiceByCompanyIdController } from "@modules/services/useCases/findServiceByCompanyId/FindServiceByCompanyIdController";
 import { FindServiceByIdController } from "@modules/services/useCases/findServiceById/FindServiceByIdController";
 import { ListServiceController } from "@modules/services/useCases/listServices/ListServicesControlle";
+import { ListServicesByCompanyIdController } from "@modules/services/useCases/listServicesByCompanyId/ListServicesByCompanyIdController";
 import { UpdateServiceByIdController } from "@modules/services/useCases/updateServiceById/UpdateServiceByIdController";
 import { Router } from "express";
+
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticate } from "../middlewares/ensureAuthenticate";
 
 export const servicesRoutes = Router();
 
@@ -12,25 +15,48 @@ const createServiceController = new CreateServiceController();
 
 const listServiceController = new ListServiceController();
 
+const listServicesByCompanyIdController =
+    new ListServicesByCompanyIdController();
+
 const updateServiceByIdController = new UpdateServiceByIdController();
 
 const findServiceByIdController = new FindServiceByIdController();
 
-const findServiceByCompanyIdController = new FindServiceByCompanyIdController();
-
 const deleteServiceByIdController = new DeleteServiceByIdController();
 
-servicesRoutes.post("/:idCompanys", createServiceController.handle);
-
-servicesRoutes.get("/", listServiceController.handle);
-
-servicesRoutes.get("/:idServices", findServiceByIdController.handle);
-
+servicesRoutes.post(
+    "/:idCompanys",
+    ensureAuthenticate,
+    ensureAdmin,
+    createServiceController.handle
+);
+servicesRoutes.get(
+    "/",
+    ensureAuthenticate,
+    ensureAdmin,
+    listServiceController.handle
+);
 servicesRoutes.get(
     "/companys/:idCompanys",
-    findServiceByCompanyIdController.handle
+    ensureAuthenticate,
+    ensureAdmin,
+    listServicesByCompanyIdController.handle
 );
-
-servicesRoutes.put("/:idServices", updateServiceByIdController.handle);
-
-servicesRoutes.delete("/:idServices", deleteServiceByIdController.handle);
+servicesRoutes.get(
+    "/:idServices",
+    ensureAuthenticate,
+    ensureAdmin,
+    findServiceByIdController.handle
+);
+servicesRoutes.put(
+    "/:idServices",
+    ensureAuthenticate,
+    ensureAdmin,
+    updateServiceByIdController.handle
+);
+servicesRoutes.delete(
+    "/:idServices",
+    ensureAuthenticate,
+    ensureAdmin,
+    deleteServiceByIdController.handle
+);
