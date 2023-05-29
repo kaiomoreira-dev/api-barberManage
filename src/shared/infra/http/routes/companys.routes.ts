3654c3ddb8702b226218bebc18e6c06a5a14d0ed
Kaio Moreira <kaio-dev@outlook.com>
2023-05-29 16:12:36 -0300

    add middlewares for authenticate routes Companys for only admin acess

--- src/shared/infra/http/routes/companys.routes.ts
+++ src/shared/infra/http/routes/companys.routes.ts
@@ -1,28 +1,52 @@
 import { CreateCompanyController } from "@modules/companys/useCases/createCompany/CreateCompanyController";
 import { DeleteCompanyByIdController } from "@modules/companys/useCases/deleteById/DeleteCompanysByIdController";
-import { FindCompanyByIdController } from "@modules/companys/useCases/findCompanyById/FindCompanyByIdController";
+import { FindCompanyByIdController } from "@modules/companys/useCases/findById/FindCompanyByIdController";
 import { ListCompanysController } from "@modules/companys/useCases/listCompanys/ListCompanysController";
 import { UpdateCompanyByIdController } from "@modules/companys/useCases/updateById/UpdateCompanysByIdController";
 import { Router } from "express";
+
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticate } from "../middlewares/ensureAuthenticate";
 
 export const companysRoutes = Router();
 
 const createCompanyController = new CreateCompanyController();
 
-const listCompanysController = new ListCompanysController();
-
 const findCompanyByIdController = new FindCompanyByIdController();
+
+const listCompanysController = new ListCompanysController();
 
 const updateCompanyByIdController = new UpdateCompanyByIdController();
 
 const deleteCompanyByIdController = new DeleteCompanyByIdController();
 
-companysRoutes.post("/:idUsers", createCompanyController.handle);
-
-companysRoutes.get("/", listCompanysController.handle);
-
-companysRoutes.get("/:idCompanys", findCompanyByIdController.handle);
-
-companysRoutes.put("/:idCompanys", updateCompanyByIdController.handle);
-
-companysRoutes.delete("/:idCompanys", deleteCompanyByIdController.handle);
+companysRoutes.post(
+    "/",
+    ensureAuthenticate,
+    ensureAdmin,
+    createCompanyController.handle
+);
+companysRoutes.get(
+    "/",
+    ensureAuthenticate,
+    ensureAdmin,
+    listCompanysController.handle
+);
+companysRoutes.get(
+    "/:idCompanys",
+    ensureAuthenticate,
+    ensureAdmin,
+    findCompanyByIdController.handle
+);
+companysRoutes.put(
+    "/:idCompanys",
+    ensureAuthenticate,
+    ensureAdmin,
+    updateCompanyByIdController.handle
+);
+companysRoutes.delete(
+    "/:idCompanys",
+    ensureAuthenticate,
+    ensureAdmin,
+    deleteCompanyByIdController.handle
+);
