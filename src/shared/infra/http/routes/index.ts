fb4b0afe84f67299efd4a9e59ec09c110b7a148b
Gustavo Reimche <gustavoreimche@hotmail.com>
2023-05-30 11:45:17 -0300

    update routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -5,6 +5,7 @@ import { companysRoutes } from "./companys.routes";
 import { costRoutes } from "./costs.routes";
 import { servicesRoutes } from "./services.routes";
 import { usersRoutes } from "./users.routes";
+import { clientsRoutes } from "./clients.routes";
 
 export const router = Router();
 
@@ -12,5 +13,6 @@ router.use("/api/users", usersRoutes);
 router.use("/api/companys", companysRoutes);
 router.use("/api/services", servicesRoutes);
 router.use("/api/costs", costRoutes);
+router.use("/api/clients", clientsRoutes);
 
 router.use(authenticatesRoutes);
