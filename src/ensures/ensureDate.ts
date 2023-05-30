39f3e80427638f2c0b70df7a51a6953544fe563a
Kaio Moreira <kaio-dev@outlook.com>
2023-05-30 16:47:19 -0300

    create ensure for validate date in your format

--- src/ensures/ensureDate.ts
+++ src/ensures/ensureDate.ts
@@ -1,7 +1,7 @@
 export const ensureDate = (date: Date) => {
-    const dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|[12]\d|3[01])\/\d{4}$/;
+    const dateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/;
     if (dateRegex.test(String(date))) {
-        return false;
+        return true;
     }
-    return true;
+    return false;
 };
