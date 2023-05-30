8a05e482c890287eed81e23df39984343da3f56b
Kaio Moreira <kaio-dev@outlook.com>
2023-05-29 21:02:42 -0300

    change connection mongodb

--- src/shared/infra/mongoose/index.ts
+++ src/shared/infra/mongoose/index.ts
@@ -1,29 +1,29 @@
 /* eslint-disable import/export */
-import { AppError } from '@shared/errors/AppError';
-import 'dotenv/config';
+import { AppError } from "@shared/errors/AppError";
+import "dotenv/config";
 
-import mongoose, { Connection } from 'mongoose';
+import mongoose, { Connection } from "mongoose";
 
 export default async function connectionMongoDB(): Promise<Connection> {
-	try {
-		const url =
-			process.env.NODE_ENV === 'test'
-				? process.env.MONGODB_URL_DBTEST
-				: process.env.MONGODB_URL_BARBERMANAGE_GUSTAVO;
+    try {
+        const url =
+            process.env.NODE_ENV === "test"
+                ? process.env.MONGODB_URL_DBTEST
+                : process.env.MONGODB_URL_BARBERMANAGE;
 
-		await mongoose.connect(url);
+        await mongoose.connect(url);
 
-		const { connection } = mongoose;
+        const { connection } = mongoose;
 
-		connection.on('open', () => {
-			console.log('Conectado ao MongoDB com sucesso!');
-		});
+        connection.on("open", () => {
+            console.log("Conectado ao MongoDB com sucesso!");
+        });
 
-		return connection;
-	} catch (error) {
-		console.log(error.message);
-		throw new AppError('Erro ao conectar ao MongoDB');
-	}
+        return connection;
+    } catch (error) {
+        console.log(error.message);
+        throw new AppError("Erro ao conectar ao MongoDB");
+    }
 }
 
 connectionMongoDB();
