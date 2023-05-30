49beec1e4e878dc15159faa862ebe80111507e1b
Kaio Moreira <kaio-dev@outlook.com>
2023-05-29 21:04:21 -0300

    change code for aspa dupla

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,13 +1,16 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { companysRoutes } from './companys.routes';
-import { servicesRoutes } from './services.routes';
-import { usersRoutes } from './users.routes';
-import { costRoutes } from './costs.routes';
+import { authenticatesRoutes } from "./authenticate.routes";
+import { companysRoutes } from "./companys.routes";
+import { costRoutes } from "./costs.routes";
+import { servicesRoutes } from "./services.routes";
+import { usersRoutes } from "./users.routes";
 
 export const router = Router();
 
-router.use('/api/users', usersRoutes);
-router.use('/api/companys', companysRoutes);
-router.use('/api/services', servicesRoutes);
-router.use('/api/costs', costRoutes);
+router.use("/api/users", usersRoutes);
+router.use("/api/companys", companysRoutes);
+router.use("/api/services", servicesRoutes);
+router.use("/api/costs", costRoutes);
+
+router.use(authenticatesRoutes);
