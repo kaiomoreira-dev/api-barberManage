006302cc785fd14cfd117353b153699a81a24f65
Kaio Moreira <kaio-dev@outlook.com>
2023-05-29 21:15:21 -0300

    resolve conflict in cost

--- src/shared/infra/http/routes/costs.routes.ts
+++ src/shared/infra/http/routes/costs.routes.ts
@@ -1,9 +1,8 @@
-import { CreateCostController } from '@modules/costs/useCases/createCost/CreateCostController';
-import { DeleteCostByIdController } from '@modules/costs/useCases/deleteById/DeleteCostByIdController';
-import { FindCostByIdController } from '@modules/costs/useCases/findById/FindByIdController';
-import { ListByIdCompanysController } from '@modules/costs/useCases/listByIdCompanys/ListByIdCompanysController';
-
-import { Router } from 'express';
+import { CreateCostController } from "@modules/costs/useCases/createCost/CreateCostController";
+import { DeleteCostByIdController } from "@modules/costs/useCases/deleteById/DeleteCostByIdController";
+import { FindCostByIdController } from "@modules/costs/useCases/findById/FindByIdController";
+import { ListByIdCompanysController } from "@modules/costs/useCases/listByIdCompanys/ListByIdCompanysController";
+import { Router } from "express";
 
 export const costRoutes = Router();
 
@@ -12,7 +11,7 @@ const listByIdCompanysController = new ListByIdCompanysController();
 const findByIdController = new FindCostByIdController();
 const deleteCostByIdController = new DeleteCostByIdController();
 
-costRoutes.post('/:idCompanys', createCostController.handle);
-costRoutes.get('/:idCost', findByIdController.handle);
-costRoutes.get('/idCompany/:idCompanys', listByIdCompanysController.handle);
-costRoutes.delete('/:idCost', deleteCostByIdController.handle);
+costRoutes.post("/:idCompanys", createCostController.handle);
+costRoutes.get("/:idCost", findByIdController.handle);
+costRoutes.get("/idCompany/:idCompanys", listByIdCompanysController.handle);
+costRoutes.delete("/:idCost", deleteCostByIdController.handle);
