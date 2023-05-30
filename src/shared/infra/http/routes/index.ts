708cba27f8a1d7cc25d12b7cecaff699b21317e4
Kaio Moreira <kaio-dev@outlook.com>
2023-05-30 15:02:16 -0300

    create route post for create serviceExecuted and create index routes for serviceExecuted

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { authenticatesRoutes } from "./authenticate.routes";
 import { companysRoutes } from "./companys.routes";
 import { costRoutes } from "./costs.routes";
+import { servicesExcuteds } from "./serviceExecuted.routes";
 import { servicesRoutes } from "./services.routes";
 import { usersRoutes } from "./users.routes";
 
@@ -12,5 +13,6 @@ router.use("/api/users", usersRoutes);
 router.use("/api/companys", companysRoutes);
 router.use("/api/services", servicesRoutes);
 router.use("/api/costs", costRoutes);
+router.use("/api/serviceExecuted", servicesExcuteds);
 
 router.use(authenticatesRoutes);
