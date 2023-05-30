8f83ed7b4245afc10e3374fe974015e9c36e6278
Gustavo Reimche <gustavoreimche@hotmail.com>
2023-05-30 09:32:13 -0300

    create UpdateCostByIdController

---
+++ src/modules/costs/useCases/updateById/UpdateCostByIdController.ts
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { UpdateCostByIdUseCase } from "./UpdateCostByIdUseCase";
+
+export class UpdateCostByIdController {
+	async handle(request: Request, response: Response): Promise<Response> {
+		const { description, idCompanys, costDate, value } = request.body;
+		const { idCost } = request.params;
+
+		const updateCostByIdUseCase = container.resolve(UpdateCostByIdUseCase);
+
+		await updateCostByIdUseCase.execute({
+			id: idCost,
+			description,
+			idCompanys,
+			costDate,
+			value,
+		});
+
+		return response.status(200).json({ message: "Updated Cost successfully." });
+	}
+}
