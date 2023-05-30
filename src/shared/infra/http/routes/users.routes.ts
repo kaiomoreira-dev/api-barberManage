224702bd793af285c4da7dbe7af1c30f1ae77546
Kaio Moreira <kaio-dev@outlook.com>
2023-05-29 21:03:57 -0300

    create route for list all user, delete and update

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,5 +1,7 @@
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { DeleteUserByIdController } from "@modules/accounts/useCases/deleteById/DeleteUserByIdController";
 import { ListUsersByCompanyIdController } from "@modules/accounts/useCases/listUserByCompaniId/ListUsersByCompaniIdController";
+import { ListUsersController } from "@modules/accounts/useCases/listUsers/ListUsersController";
 import { UpdateUsersController } from "@modules/accounts/useCases/updateUser/UpdateUsersController";
 import { Router } from "express";
 
@@ -14,15 +16,39 @@ const listUsersByCompanyIdController = new ListUsersByCompanyIdController();
 
 const updateUsersController = new UpdateUsersController();
 
+const deleteUserByIdController = new DeleteUserByIdController();
+
+const listUsersController = new ListUsersController();
+
 usersRoutes.post("/", createUserController.handle);
+
+usersRoutes.get(
+    "/",
+    ensureAuthenticate,
+    ensureAdmin,
+    listUsersController.handle
+);
 
 usersRoutes.get(
     "/:idCompanys",
     ensureAuthenticate,
+    ensureAdmin,
     listUsersByCompanyIdController.handle
 );
 
-usersRoutes.put("/", ensureAuthenticate, updateUsersController.handle);
+usersRoutes.put(
+    "/",
+    ensureAuthenticate,
+    ensureAdmin,
+    updateUsersController.handle
+);
+
+usersRoutes.delete(
+    "/:idUsers",
+    ensureAuthenticate,
+    ensureAdmin,
+    deleteUserByIdController.handle
+);
 
 // criar rota para atualizar qualquer user para administrador
 // Mas com apenas permissão de desenvolvedor
