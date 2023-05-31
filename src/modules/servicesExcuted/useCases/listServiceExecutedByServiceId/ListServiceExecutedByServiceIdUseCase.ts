8fcfd515a29f5f57a1693ed6ecd0e4a5c635689f
Kaio Moreira <kaio-dev@outlook.com>
2023-05-31 00:01:12 -0300

    create use case for list service executed by service ID

---
+++ src/modules/servicesExcuted/useCases/listServiceExecutedByServiceId/ListServiceExecutedByServiceIdUseCase.ts
@@ -0,0 +1,34 @@
+import { ICompanysRepository } from "@modules/companys/repositories/ICompanysRepository";
+import { IServicesRepository } from "@modules/services/repositories/IServicesRepository";
+import { IServiceExecutedModel } from "@modules/servicesExcuted/infra/mongoose/entities/ServiceExecuted";
+import { IServiceExecutedRepository } from "@modules/servicesExcuted/repositories/IServiceExecutedRepository";
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@shared/errors/AppError";
+
+@injectable()
+export class ListServiceExecutedByServiceIdUseCase {
+    constructor(
+        @inject("ServiceExecutedRepository")
+        private serviceExecutedRepository: IServiceExecutedRepository,
+        @inject("ServicesRepository")
+        private servicesRepository: IServicesRepository
+    ) {}
+
+    async execute(idServices: string): Promise<IServiceExecutedModel[]> {
+        const checkCompanyExist = await this.servicesRepository.findById(
+            idServices
+        );
+
+        if (!checkCompanyExist) {
+            throw new AppError("Service not found", 404);
+        }
+
+        const servicesExecuted =
+            await this.serviceExecutedRepository.listByServiceId(
+                checkCompanyExist.id
+            );
+
+        return servicesExecuted;
+    }
+}
