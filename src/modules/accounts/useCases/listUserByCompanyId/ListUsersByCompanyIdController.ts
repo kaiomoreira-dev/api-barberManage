2e0fde934cfc6039d08e07f9a3a9e3e230c69c04
Kaio Moreira <kaio-dev@outlook.com>
2023-05-31 11:47:46 -0300

    alter name case for listUserByCompanyId

---
+++ src/modules/accounts/useCases/listUserByCompanyId/ListUsersByCompanyIdController.ts
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { ListUsersByCompanyIdUseCase } from "./ListUsersByCompanyIdUseCase";
+
+export class ListUsersByCompanyIdController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { idCompanys } = request.params;
+
+        const listUsersByCompanyIdUseCase = container.resolve(
+            ListUsersByCompanyIdUseCase
+        );
+        const users = await listUsersByCompanyIdUseCase.execute(idCompanys);
+
+        return response.status(200).json(users);
+    }
+}
