e28f10cadefe07cf10e47f4e5d3d9da1534c7f05
Kaio Moreira <kaio-dev@outlook.com>
2023-05-31 00:03:37 -0300

    remove code for error in update company

--- src/modules/companys/useCases/updateById/UpdateCompanysByIdUseCase.ts
+++ src/modules/companys/useCases/updateById/UpdateCompanysByIdUseCase.ts
@@ -10,43 +10,40 @@ import { AppError } from "@shared/errors/AppError";
 
 @injectable()
 export class UpdateCompanyByIdUseCase {
-	constructor(
-		@inject("CompanysRepository")
-		private companysRepository: ICompanysRepository
-	) {}
+    constructor(
+        @inject("CompanysRepository")
+        private companysRepository: ICompanysRepository
+    ) {}
 
-	async execute({ id, address, name, phone }: ICreateCompanysDTO): Promise<void> {
-		const checkCompanyExist = await this.companysRepository.findById(id);
-		if (!checkCompanyExist) {
-			throw new AppError("Company not found", 404);
-		}
+    async execute({
+        id,
+        address,
+        name,
+        phone,
+    }: ICreateCompanysDTO): Promise<void> {
+        const checkCompanyExist = await this.companysRepository.findById(id);
+        if (!checkCompanyExist) {
+            throw new AppError("Company not found", 404);
+        }
 
-		if (!ensureName(name)) {
-			throw new AppError("Name is not available", 401);
-		}
+        if (!ensureName(name)) {
+            throw new AppError("Name is not available", 401);
+        }
 
-		// const checkCompanyExists = await this.companysRepository.findByName(
-		//     name
-		// );
+        if (!ensureAddress(address)) {
+            throw new AppError("address is not available", 401);
+        }
 
-		// if (checkCompanyExists) {
-		//     throw new AppError("Company already exists", 401);
-		// }
+        // add test for phone in jest
+        if (!ensurePhone(phone)) {
+            throw new AppError("phone is not available", 401);
+        }
 
-		if (!ensureAddress(address)) {
-			throw new AppError("address is not available", 401);
-		}
-
-		// add test for phone in jest
-		if (!ensurePhone(phone)) {
-			throw new AppError("phone is not available", 401);
-		}
-
-		await this.companysRepository.updateById({
-			id,
-			name,
-			address,
-			phone,
-		});
-	}
+        await this.companysRepository.updateById({
+            id,
+            name,
+            address,
+            phone,
+        });
+    }
 }
