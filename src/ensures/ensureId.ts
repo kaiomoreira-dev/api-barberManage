4ce0dd60b5ae0b5c47b5c474f7199e2d8d5a8cd5
Kaio Moreira <kaio-dev@outlook.com>
2023-05-31 00:05:34 -0300

    remove for validate null id

--- src/ensures/ensureId.ts
+++ src/ensures/ensureId.ts
@@ -1,10 +1,9 @@
 /* eslint-disable no-useless-escape */
 export const ensureId = (id: string) => {
-    if (!id) {
-        return true;
-    }
-
     const idRegex = /^[0-9a-f]{24}$/;
 
-    return idRegex.test(id);
+    if (idRegex.test(id)) {
+        return false;
+    }
+    return true;
 };
