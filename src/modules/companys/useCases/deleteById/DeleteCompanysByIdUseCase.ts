e976ddffc903e2401c07560c79cbff9d4b979ce6
Kaio Moreira <kaio-dev@outlook.com>
2023-06-01 20:17:04 -0300

    add ensuresID for validation ID

--- src/modules/companys/useCases/deleteById/DeleteCompanysByIdUseCase.ts
+++ src/modules/companys/useCases/deleteById/DeleteCompanysByIdUseCase.ts
@@ -1,3 +1,5 @@
+import { ensureId } from "@ensures/ensureId";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { ICreateCompanysDTO } from "@modules/companys/dtos/ICreateCompanysDTO";
 import { ICompanysRepository } from "@modules/companys/repositories/ICompanysRepository";
 import { inject, injectable } from "tsyringe";
@@ -8,10 +10,16 @@ import { AppError } from "@shared/errors/AppError";
 export class DeleteCompanyByIdUseCase {
     constructor(
         @inject("CompanysRepository")
-        private companysRepository: ICompanysRepository
+        private companysRepository: ICompanysRepository,
+        @inject("UsersRepository")
+        private userRepository: IUsersRepository
     ) {}
 
     async execute({ id }: ICreateCompanysDTO): Promise<void> {
+        if (!ensureId(id)) {
+            throw new AppError("Company not found", 404);
+        }
+
         const checkCompanyExist = await this.companysRepository.findById(id);
 
         if (!checkCompanyExist) {
@@ -19,5 +27,7 @@ export class DeleteCompanyByIdUseCase {
         }
 
         await this.companysRepository.deleteById(id);
+
+        await this.userRepository.updateCompanysIdsByCompanyId(id);
     }
 }
