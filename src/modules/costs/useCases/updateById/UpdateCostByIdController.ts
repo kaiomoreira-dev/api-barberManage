20845f8fd51e0fcd9310550189d35e28fd1601cc
Kaio Moreira <kaio-dev@outlook.com>
2023-06-01 20:01:23 -0300

    alter icompatible lines

--- src/modules/costs/useCases/updateById/UpdateCostByIdController.ts
+++ src/modules/costs/useCases/updateById/UpdateCostByIdController.ts
@@ -5,20 +5,22 @@ import { container } from "tsyringe";
 import { UpdateCostByIdUseCase } from "./UpdateCostByIdUseCase";
 
 export class UpdateCostByIdController {
-	async handle(request: Request, response: Response): Promise<Response> {
-		const { description, idCompanys, costDate, value } = request.body;
-		const { idCost } = request.params;
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { description, idCompanys, costDate, value } = request.body;
+        const { idCost } = request.params;
 
-		const updateCostByIdUseCase = container.resolve(UpdateCostByIdUseCase);
+        const updateCostByIdUseCase = container.resolve(UpdateCostByIdUseCase);
 
-		await updateCostByIdUseCase.execute({
-			id: idCost,
-			description,
-			idCompanys,
-			costDate,
-			value,
-		});
+        await updateCostByIdUseCase.execute({
+            id: idCost,
+            description,
+            idCompanys,
+            costDate,
+            value,
+        });
 
-		return response.status(200).json({ message: "Updated Cost successfully." });
-	}
+        return response
+            .status(200)
+            .json({ message: "Updated Cost successfully." });
+    }
 }
