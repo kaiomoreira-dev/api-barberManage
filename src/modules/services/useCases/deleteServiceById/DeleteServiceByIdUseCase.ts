2bd25664b2634c5a4031947c1a5b0add4dfbdb82
Kaio Moreira <kaio-dev@outlook.com>
2023-06-01 19:48:21 -0300

    add ensureID for valid id Service

--- src/modules/services/useCases/deleteServiceById/DeleteServiceByIdUseCase.ts
+++ src/modules/services/useCases/deleteServiceById/DeleteServiceByIdUseCase.ts
@@ -1,3 +1,4 @@
+import { ensureId } from "@ensures/ensureId";
 import { ICreateServiceDTO } from "@modules/services/dtos/ICreateServiceDTO";
 import { IServicesRepository } from "@modules/services/repositories/IServicesRepository";
 import { inject, injectable } from "tsyringe";
@@ -12,6 +13,10 @@ export class DeleteServiceByIdUseCase {
     ) {}
 
     async execute({ id }: ICreateServiceDTO): Promise<void> {
+        if (!ensureId(id)) {
+            throw new AppError("Service not found", 404);
+        }
+
         const checkServiceExists = this.servicesRepository.findById(id);
 
         if (!checkServiceExists) {
