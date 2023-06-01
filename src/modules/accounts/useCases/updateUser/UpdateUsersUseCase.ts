d5e9ce2071d82952a760d7f3e578ffdd7b37619b
Kaio Moreira <kaio-dev@outlook.com>
2023-06-01 20:37:48 -0300

    add ensureID for validation id and logic for validation all id company

--- src/modules/accounts/useCases/updateUser/UpdateUsersUseCase.ts
+++ src/modules/accounts/useCases/updateUser/UpdateUsersUseCase.ts
@@ -1,5 +1,9 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-await-in-loop */
+import { ensureId } from "@ensures/ensureId";
 import { ICreateUsersDTO } from "@modules/accounts/dtos/ICreateUsersDTO";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { ICompanysRepository } from "@modules/companys/repositories/ICompanysRepository";
 import { inject, injectable } from "tsyringe";
 
 import { AppError } from "@shared/errors/AppError";
@@ -8,7 +12,9 @@ import { AppError } from "@shared/errors/AppError";
 export class UpdateUsersUseCase {
     constructor(
         @inject("UsersRepository")
-        private userRepository: IUsersRepository
+        private userRepository: IUsersRepository,
+        @inject("CompanysRepository")
+        private companysRepository: ICompanysRepository
     ) {}
     async execute({
         id,
@@ -16,19 +22,32 @@ export class UpdateUsersUseCase {
         email,
         address,
         phone,
+        idCompanys,
     }: ICreateUsersDTO): Promise<void> {
+        if (!ensureId(id)) {
+            throw new AppError("User not found", 401);
+        }
         const checkUserdExists = this.userRepository.findById(id);
 
         if (!checkUserdExists) {
             throw new AppError("User not found", 404);
         }
 
+        for (const company of idCompanys) {
+            const checkCompanyExists = await this.companysRepository.findById(
+                String(company)
+            );
+            if (!checkCompanyExists) {
+                throw new AppError("Company not found", 404);
+            }
+        }
         await this.userRepository.updateById({
             id,
             name,
             email,
             address,
             phone,
+            idCompanys,
         });
     }
 }
