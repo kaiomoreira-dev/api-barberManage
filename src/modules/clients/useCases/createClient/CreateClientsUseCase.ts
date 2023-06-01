82f1366e639461aaa5c199df73b6428b5f755f18
Kaio Moreira <kaio-dev@outlook.com>
2023-06-01 20:31:43 -0300

    add ensures for validation informations atributos

--- src/modules/clients/useCases/createClient/CreateClientsUseCase.ts
+++ src/modules/clients/useCases/createClient/CreateClientsUseCase.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-unresolved */
 import { ensureAddress } from "@ensures/ensureAddress";
 import { ensureEsqd } from "@ensures/ensureEsqd";
+import { ensureId } from "@ensures/ensureId";
 import { ensureName } from "@ensures/ensureName";
 import { ensureNumber } from "@ensures/ensureNumber";
 import { ensurePG } from "@ensures/ensurePG";
@@ -15,69 +16,76 @@ import { AppError } from "@shared/errors/AppError";
 
 @injectable()
 export class CreateClientsUseCase {
-	constructor(
-		@inject("ClientsRepository")
-		private clientsRepository: IClientsRepository,
-		@inject("CompanysRepository")
-		private companysRepository: ICompanysRepository
-	) {}
+    constructor(
+        @inject("ClientsRepository")
+        private clientsRepository: IClientsRepository,
+        @inject("CompanysRepository")
+        private companysRepository: ICompanysRepository
+    ) {}
 
-	async execute({
-		idCompanys,
-		name,
-		address,
-		esqd,
-		num,
-		pg,
-		phone,
-	}: ICreateClientDTO): Promise<IClientModel> {
-		const checkCompanyExist = await this.companysRepository.findById(idCompanys);
+    async execute({
+        idCompanys,
+        name,
+        address,
+        esqd,
+        num,
+        pg,
+        phone,
+    }: ICreateClientDTO): Promise<IClientModel> {
+        if (!ensureId(idCompanys)) {
+            throw new AppError("Client not found", 404);
+        }
+        const checkCompanyExist = await this.companysRepository.findById(
+            idCompanys
+        );
 
-		if (!checkCompanyExist) {
-			throw new AppError("Company not found", 404);
-		}
+        if (!checkCompanyExist) {
+            throw new AppError("Company not found", 404);
+        }
 
-		if (!ensureName(name)) {
-			throw new AppError("Name is not available", 401);
-		}
+        if (!ensureName(name)) {
+            throw new AppError("Name is not available", 401);
+        }
 
-		const checkCompanyExists = await this.clientsRepository.findByName(name);
+        const checkCompanyExists = await this.clientsRepository.findByName(
+            name
+        );
 
-		if (checkCompanyExists) {
-			throw new AppError("Client already exists", 401);
-		}
+        if (checkCompanyExists) {
+            throw new AppError("Client already exists", 401);
+        }
 
-		if (!ensureAddress(address)) {
-			throw new AppError("Adress is not available", 401);
-		}
+        if (!ensureAddress(address)) {
+            throw new AppError("Adress is not available", 401);
+        }
 
-		// add test for phone in jest
-		// if (!ensurePhone(phone)) {
-		//     throw new AppError("Phone is not available", 401);
-		// }
+        // add test for phone in jest
+        if (!ensurePhone(phone)) {
+            throw new AppError("Phone is not available", 401);
+        }
 
-		if (!ensureNumber(num)) {
-			throw new AppError("Number is not available", 401);
-		}
+        if (!ensureNumber(num)) {
+            throw new AppError("Number is not available", 401);
+        }
 
-		if (!ensureEsqd(esqd)) {
-			throw new AppError("Esqd is not available", 401);
-		}
+        if (!ensureEsqd(esqd)) {
+            throw new AppError("Esqd is not available", 401);
+        }
 
-		if (!ensurePG(pg)) {
-			throw new AppError("Pg is not available", 401);
-		}
+        if (!ensurePG(pg)) {
+            throw new AppError("Pg is not available", 401);
+        }
 
-		const client = await this.clientsRepository.create({
-			idCompanys,
-			name,
-			address,
-			esqd,
-			num,
-			pg,
-			phone,
-		});
+        const client = await this.clientsRepository.create({
+            idCompanys,
+            name,
+            address,
+            esqd,
+            num,
+            pg,
+            phone,
+        });
 
-		return client;
-	}
+        return client;
+    }
 }
