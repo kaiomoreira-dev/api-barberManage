3fcf115d947b9f4cb6d8bfc8bbc065a20e7e14cc
Kaio Moreira <kaio-dev@outlook.com>
2023-06-01 20:03:03 -0300

    add ensuresID for validation and verificaiton in bd

--- src/modules/costs/useCases/listByIdCompanys/ListByIdCompnaysUseCase.ts
+++ src/modules/costs/useCases/listByIdCompanys/ListByIdCompnaysUseCase.ts
@@ -1,31 +1,35 @@
-import { ensureName } from 'ensures/ensureName';
-import { ICreateCompanysDTO } from '@modules/companys/dtos/ICreateCompanysDTO';
-import { ICompanyModel } from '@modules/companys/infra/mongoose/entities/Companys';
-import { ICompanysRepository } from '@modules/companys/repositories/ICompanysRepository';
-import { inject, injectable } from 'tsyringe';
+import { ensureId } from "@ensures/ensureId";
+import { ICreateCompanysDTO } from "@modules/companys/dtos/ICreateCompanysDTO";
+import { ICompanyModel } from "@modules/companys/infra/mongoose/entities/Companys";
+import { ICompanysRepository } from "@modules/companys/repositories/ICompanysRepository";
+import { ICostModel } from "@modules/costs/infra/entities/Cost";
+import { ICostsRepository } from "@modules/costs/repositories/ICostsRepository";
+import { ensureName } from "ensures/ensureName";
+import { inject, injectable } from "tsyringe";
 
-import { AppError } from '@shared/errors/AppError';
-import { ICostsRepository } from '@modules/costs/repositories/ICostsRepository';
-import { ICostModel } from '@modules/costs/infra/entities/Cost';
+import { AppError } from "@shared/errors/AppError";
 
 @injectable()
 export class ListByIdCompanysUseCase {
-	constructor(
-		@inject('CostsRepository')
-		private costsRepository: ICostsRepository,
-		@inject('CompanysRepository')
-		private companysRepository: ICompanysRepository
-	) {}
+    constructor(
+        @inject("CostsRepository")
+        private costsRepository: ICostsRepository,
+        @inject("CompanysRepository")
+        private companysRepository: ICompanysRepository
+    ) {}
 
-	async execute({ id }: ICreateCompanysDTO): Promise<ICostModel[]> {
-		const checkCompanyExist = await this.companysRepository.findById(id);
+    async execute({ id }: ICreateCompanysDTO): Promise<ICostModel[]> {
+        if (!ensureId(id)) {
+            throw new AppError("Service not found", 404);
+        }
+        const checkCompanyExist = await this.companysRepository.findById(id);
 
-		if (!checkCompanyExist) {
-			throw new AppError('Company not found', 404);
-		}
+        if (!checkCompanyExist) {
+            throw new AppError("Company not found", 404);
+        }
 
-		const costs = await this.costsRepository.listByIdCompany(id);
+        const costs = await this.costsRepository.listByIdCompany(id);
 
-		return costs;
-	}
+        return costs;
+    }
 }
