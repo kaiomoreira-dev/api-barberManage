82db5702edfcf37f62786ad8516d4b18b24c57c4
Kaio Moreira <kaio-dev@outlook.com>
2023-06-01 17:56:12 -0300

    alter logic for list all serviceExcuted by User with return object full when find list

--- src/modules/servicesExcuted/useCases/listServiceExecutedByUserId/ListServiceExecutedByUserIdUseCase.ts
+++ src/modules/servicesExcuted/useCases/listServiceExecutedByUserId/ListServiceExecutedByUserIdUseCase.ts
@@ -1,4 +1,15 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ensureId } from "@ensures/ensureId";
+import { UserMap } from "@modules/accounts/mappers/UserMap";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { IClientsRepository } from "@modules/clients/repositories/IClientsRepository";
+import { ICompanysRepository } from "@modules/companys/repositories/ICompanysRepository";
+import { IServiceModel } from "@modules/services/infra/mongoose/entities/Services";
 import { IServicesRepository } from "@modules/services/repositories/IServicesRepository";
+import { IResponseServiceExecuted } from "@modules/servicesExcuted/dtos/IResponseServiceExecutedDTO";
 import { IServiceExecutedModel } from "@modules/servicesExcuted/infra/mongoose/entities/ServiceExecuted";
 import { IServiceExecutedRepository } from "@modules/servicesExcuted/repositories/IServiceExecutedRepository";
 import { inject, injectable } from "tsyringe";
@@ -6,28 +17,83 @@ import { inject, injectable } from "tsyringe";
 import { AppError } from "@shared/errors/AppError";
 
 @injectable()
-export class ListServiceExecutedByServiceIdUseCase {
+export class ListServiceExecutedByUserIdUseCase {
     constructor(
         @inject("ServiceExecutedRepository")
         private serviceExecutedRepository: IServiceExecutedRepository,
+        @inject("ClientsRepository")
+        private clientsRepository: IClientsRepository,
+        @inject("CompanysRepository")
+        private companysRepository: ICompanysRepository,
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository,
         @inject("ServicesRepository")
         private servicesRepository: IServicesRepository
     ) {}
 
-    async execute(idServices: string): Promise<IServiceExecutedModel[]> {
-        const checkCompanyExist = await this.servicesRepository.findById(
-            idServices
-        );
+    async execute(idUsers: string): Promise<IResponseServiceExecuted[]> {
+        if (!ensureId(idUsers)) {
+            throw new AppError("User not found", 404);
+        }
 
-        if (!checkCompanyExist) {
-            throw new AppError("Service not found", 404);
+        const checkUsersExist = await this.usersRepository.findById(idUsers);
+
+        if (!checkUsersExist) {
+            throw new AppError("User not found", 404);
         }
 
         const servicesExecuted =
-            await this.serviceExecutedRepository.listByServiceId(
-                checkCompanyExist.id
+            await this.serviceExecutedRepository.listByUserId(idUsers);
+
+        const allServicesExecutedByClient: IResponseServiceExecuted[] = [];
+        const allServicesRequired: IServiceModel[] = [];
+
+        for (const service of servicesExecuted) {
+            for (const serviceIdRequired of service.idServices) {
+                const findService = await this.servicesRepository.findById(
+                    String(serviceIdRequired)
+                );
+                allServicesRequired.push(findService);
+            }
+            const company = await this.companysRepository.findById(
+                String(service.idCompanys)
             );
 
-        return servicesExecuted;
+            const client = await this.clientsRepository.findById(
+                String(service.idClients)
+            );
+            const idCompanys: string[] = [];
+            for (const company of checkUsersExist.idCompanys) {
+                idCompanys.push(String(company));
+            }
+
+            const serviceExecuted: IResponseServiceExecuted = {
+                id: service._id,
+                company,
+                user: UserMap.toDTO({
+                    id: checkUsersExist.id,
+                    name: checkUsersExist.name,
+                    email: checkUsersExist.email,
+                    phone: checkUsersExist.phone,
+                    address: checkUsersExist.address,
+                    createdAt: checkUsersExist.createdAt,
+                    updatedAt: checkUsersExist.updatedAt,
+                    admin: checkUsersExist.admin,
+                    employee: checkUsersExist.employee,
+                    idCompanys,
+                }),
+                client,
+                services: allServicesRequired,
+                isLogged: service.isLogged,
+                value: service.value,
+                paymentMethod: service.paymentMethod,
+                paymentDate: service.paymentDate,
+                serviceDate: service.serviceDate,
+            };
+
+            allServicesExecutedByClient.push(serviceExecuted);
+        }
+
+        return allServicesExecutedByClient;
     }
 }
