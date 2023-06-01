e58f2c87c2ae99da7d64aae6e7dc2133454e1bdb
Kaio Moreira <kaio-dev@outlook.com>
2023-06-01 20:04:10 -0300

    add ensuresID for validation and verificaiton in bd

--- src/modules/costs/useCases/findById/FindByIdUseCase.ts
+++ src/modules/costs/useCases/findById/FindByIdUseCase.ts
@@ -1,24 +1,29 @@
-import { inject, injectable } from 'tsyringe';
+import { ensureId } from "@ensures/ensureId";
+import { ICreateCostsDTO } from "@modules/costs/dtos/ICreateCostsDTO";
+import { ICostModel } from "@modules/costs/infra/entities/Cost";
+import { ICostsRepository } from "@modules/costs/repositories/ICostsRepository";
+import { inject, injectable } from "tsyringe";
 
-import { AppError } from '@shared/errors/AppError';
-import { ICostsRepository } from '@modules/costs/repositories/ICostsRepository';
-import { ICostModel } from '@modules/costs/infra/entities/Cost';
-import { ICreateCostsDTO } from '@modules/costs/dtos/ICreateCostsDTO';
+import { AppError } from "@shared/errors/AppError";
 
 @injectable()
 export class FindCostByIdUseCase {
-	constructor(
-		@inject('CostsRepository')
-		private costRepository: ICostsRepository
-	) {}
+    constructor(
+        @inject("CostsRepository")
+        private costRepository: ICostsRepository
+    ) {}
 
-	async execute({ id }: ICreateCostsDTO): Promise<ICostModel> {
-		const checkCostExists = this.costRepository.findById(id);
+    async execute({ id }: ICreateCostsDTO): Promise<ICostModel> {
+        if (!ensureId(id)) {
+            throw new AppError("Service not found", 404);
+        }
 
-		if (!checkCostExists) {
-			throw new AppError('Cost not found', 404);
-		}
+        const checkCostExists = this.costRepository.findById(id);
 
-		return checkCostExists;
-	}
+        if (!checkCostExists) {
+            throw new AppError("Cost not found", 404);
+        }
+
+        return checkCostExists;
+    }
 }
