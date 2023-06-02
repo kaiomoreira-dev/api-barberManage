31f45ae498a563d6635f693d37566fefcd58d1a0
Kaio Moreira <kaio-dev@outlook.com>
2023-06-02 13:20:39 -0300

    update expression regular

--- src/ensures/ensureDate.ts
+++ src/ensures/ensureDate.ts
@@ -1,5 +1,7 @@
 export const ensureDate = (date: Date) => {
-    const dateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/;
+    const dateRegex =
+        /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})\.(\d{3})Z$/;
+
     if (dateRegex.test(String(date))) {
         return true;
     }
