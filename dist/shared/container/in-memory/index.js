5001511708ef8c6bd2163bfa7af4347e19604b6e
Kaio Moreira <kaio-dev@outlook.com>
2023-06-03 12:11:46 -0300

    remove builder fodler dist

--- dist/shared/container/in-memory/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var _UsersRepositoryInMemory = require("../../../modules/accounts/repositories/in-memory/UsersRepositoryInMemory");
-var _tsyringe = require("tsyringe");
-_tsyringe.container.registerSingleton("UsersRepositoryInMemory", _UsersRepositoryInMemory.UsersRepositoryInMemory);
