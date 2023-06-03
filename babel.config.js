ae47fb34b7e976f8a7191d98f1180bc8e85072b2
Kaio Moreira <kaio-dev@outlook.com>
2023-06-03 12:11:19 -0300

    create babel config for ensures path

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-    presets: [
-      ["@babel/preset-env", { targets: { node: "current" } }],
-      "@babel/preset-typescript",
-    ],
-    plugins: [
-      [
-        "module-resolver",
-        {
-          alias: {
-            "@modules": "./src/modules",
-            "@shared": "./src/shared",
-            "@erros": "./src/errors",
-            "@config": "./src/config",
-            "@utils": "./src/utils",
-          },
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@modules": "./src/modules",
+          "@shared": "./src/shared",
+          "@erros": "./src/errors",
+          "@config": "./src/config",
+          "@utils": "./src/utils",
+          "@ensures": "./src/ensures",
         },
-      ],
-      "babel-plugin-transform-typescript-metadata",
-      ["@babel/plugin-proposal-decorators", { legacy: true }],
-      ["@babel/plugin-proposal-class-properties", { loose: true }],
+      },
     ],
-  };
-  
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+  ],
+};
