609bb443c7b337ab3c79e0f217571f92f715752a
Kaio Moreira <kaio-dev@outlook.com>
2023-06-06 15:34:27 -0300

    create interface for costs

--- src/modules/costs/repositories/ICostsRepository.ts
+++ src/modules/costs/repositories/ICostsRepository.ts
@@ -2,10 +2,10 @@ import { ICreateCostsDTO } from "../dtos/ICreateCostsDTO";
 import { ICostModel } from "../infra/entities/Cost";
 
 export interface ICostsRepository {
-	create(data: ICreateCostsDTO): Promise<ICostModel>;
-	list(): Promise<ICostModel[]>;
-	listByIdCompany(idCompanys: string): Promise<ICostModel[]>;
-	findById(id: string): Promise<ICostModel>;
-	updateById(data: ICreateCostsDTO): Promise<void>;
-	deleteById(id: string): Promise<void>;
+    create(data: ICreateCostsDTO): Promise<ICostModel>;
+    list(): Promise<ICostModel[]>;
+    listByIdCompany(idCompanys: string): Promise<ICostModel[]>;
+    findById(id: string): Promise<ICostModel>;
+    updateById(data: ICreateCostsDTO): Promise<void>;
+    deleteById(id: string): Promise<void>;
 }
