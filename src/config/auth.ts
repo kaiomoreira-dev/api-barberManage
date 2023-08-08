334831add9b08f071d4de62321a133d3183af71f
Kaio Moreira <kaio-dev@outlook.com>
2023-08-08 10:52:54 -0300

    update: converte all string secrest with JSON.parse

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
 
 export default {
-    secretToken: process.env.SECRET_TOKEN,
-    expireInToken: process.env.EXPIRE_IN_TOKEN,
-    secretRefreshToken: process.env.SECRET_REFRESH_TOKEN,
-    expireRefreshToken: process.env.EXPIRE_REFRESH_TOKEN,
+    secretToken: JSON.parse(process.env.SECRET_TOKEN),
+    expireInToken: JSON.parse(process.env.EXPIRE_IN_TOKEN),
+    secretRefreshToken: JSON.parse(process.env.SECRET_REFRESH_TOKEN),
+    expireRefreshToken: JSON.parse(process.env.EXPIRE_REFRESH_TOKEN),
     daysRefreshToken: Number(process.env.DAYS_REFRESH_TOKEN),
 };
